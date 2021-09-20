1ef5881364bb963d171e45b87702b994aeed33c9
azizsige <kurniawan00azizsige@gmail.com>
2021-09-20 10:54:13 +0700

    add element semu

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 const btnStart = document.getElementById('btn-start')
 const contentPlay = document.getElementById('content-play');
 const btnBack = document.getElementById('back');
+const selecteds = document.querySelectorAll('.selected');
 
 btnStart.addEventListener('click', function(){
   contentPlay.classList.remove('hidden')
@@ -8,3 +9,17 @@ btnStart.addEventListener('click', function(){
 btnBack.addEventListener('click', function(){
   contentPlay.classList.add('hidden')
 })
+
+selecteds.forEach(selected => {
+  selected.addEventListener('click', event => {
+    let target = event.target.id
+
+    console.log(target);
+  })
+})
+
+// -mt-32
+
+// <div class="user-selected">
+//                 <img src="./assets/paper-user.png" alt="" srcset="">
+//               </div>
