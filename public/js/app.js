306a2f64b3da28a078f80e51a8b42dfeb707019d
azizsige <kurniawan00azizsige@gmail.com>
2021-09-22 13:35:34 +0700

    make little bit interactive in start page

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,18 @@
 const btnStart = document.getElementById('btn-start')
+const imageThumb = document.getElementById('img-thumb')
+
+function welcome(){
+  let image = ['rock', 'paper', 'scissors'];
+  let count = 0;
+
+  setInterval(function(){
+    imageThumb.setAttribute('src', `./assets/${image[count++]}-start.png`)
+      if(count == image.length) count = 0
+    return;
+  }, 700)
+}
+
+welcome()
 
 btnStart.addEventListener('click', function(){
   location.href = './public/play.html';
